52dc4ae80051da9c5d1b1e9f038820f0617a16cc
Abdul Fattah <alfattahkarim@gmail.com>
2024-08-15 18:18:23 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Birthday from "./components/Birthday";
+
+function App() {
+  return (
+    <>
+      <Birthday />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Birthday.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../components/Birthday.css";
+
+const Birthday = () => {
+  return (
+    <>
+      <div className="card">
+        <div className="card-header background"></div>
+        <div className="card-body">
+          <p>We are so excited to celebrate with you on August 20th!</p>
+          <p>Let's make this day full of fun and laughter!</p>
+        </div>
+        <div className="card-footer">
+          <p>With love,</p>
+          <p>[Your Family's Name]</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Birthday;
