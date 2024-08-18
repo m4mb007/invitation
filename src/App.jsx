0573d7ddf5f26432717d719504fb8fb4c36248b3
mambo <m4mb007@gmail.com>
2024-08-18 12:13:13 +0800

    remove image slider component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import Birthday from "./components/Birthday";
 import BottomNav from "./components/BottomNav";
-import { EmblaCarousel } from "./components/imageSlider";
 
 function App() {
   return (
