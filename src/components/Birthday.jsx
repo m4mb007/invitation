657074c518a209cb89595dffef03f3a27dd8a182
m4mb007 <m4mb007@gmail.com>
2024-08-19 00:54:28 +0800

    img edit

--- src/components/Birthday.jsx
+++ src/components/Birthday.jsx
@@ -7,7 +7,7 @@ const Birthday = () => {
       <div className="card">
         <div className="card-header background"></div>
         <div className="card-body background_sec02">
-          <img src="/assets/02.png" alt="Slide 3" height="400px" />
+          <img src="/02.png" alt="Slide 3" height="400px" />
           <p>We are so excited to celebrate with you on August 24th!</p>
           <p>Let's make this day full of fun and laughter!</p>
           <EmblaCarousel />
